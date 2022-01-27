7473bab23613993a7fc8cb2317e6c40019a46eb4
Gustavo <gordono@unsa.edu.pe>
2022-01-27 10:01:03 -0500

    Making an optimization to the problem of Div 2.

--- Codeforces/MinMaxSwap.py
+++ Codeforces/MinMaxSwap.py
@@ -9,9 +9,7 @@ def solve(a, b):
 if __name__ == "__main__":
     t = int(input())
     for _ in range(t):
-        n = int(input())
-        a = input().split(" ")
-        a = list(map(int, a))
-        b = input().split(" ")
-        b = list(map(int, b))
+        n = int(input()) # This variable doesn't affect the program already
+        a = list(map(int, input().split(" ")))
+        b = list(map(int, input().split(" ")))
         print(solve(a, b))
